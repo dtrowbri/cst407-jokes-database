21e5c9e4c08f06729ee1950c04bfc7ded2e58ede
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2022-07-04 22:22:01 -0700

    Update google_login.php

--- jokesdb/google_login.php
+++ jokesdb/google_login.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-//session_start();
+session_start();
 echo 'adding google auth';
 //require_once('vendor/autoload.php');
 echo 'google auth added';
