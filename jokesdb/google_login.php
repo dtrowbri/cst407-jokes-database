ec52e6e500d8481aa3d0ffefd5f016d679e19c05
dtrowbri <73806335+dtrowbri@users.noreply.github.com>
2022-07-04 22:16:01 -0700

    done

--- jokesdb/google_login.php
+++ jokesdb/google_login.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-session_start();
+//session_start();
 echo 'adding google auth';
-require_once('vendor/autoload.php');
+//require_once('vendor/autoload.php');
 echo 'google auth added';
  /*
 $client_id = "106636728529-2i5kaeki866pua1l6bs9f7ofut0ljh67.apps.googleusercontent.com";
